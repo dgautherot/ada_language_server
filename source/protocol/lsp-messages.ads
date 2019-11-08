024f69ac99d6774cc6ff077911b46756c18846ae
Maxim Reznik <reznik@adacore.com>
2019-11-08 19:05:34 +0200

    SA21-039 Add ContentModified constant.

--- source/protocol/lsp-messages.ads
+++ source/protocol/lsp-messages.ads
@@ -134,6 +134,7 @@ package LSP.Messages is
    --
    --	// Defined by the protocol.
    --	export const RequestCancelled: number = -32800;
+   --	export const ContentModified: number = -32801;
    --}
    --```
    type ErrorCodes is
@@ -146,7 +147,8 @@ package LSP.Messages is
       serverErrorEnd,
       ServerNotInitialized,
       UnknownErrorCode,
-      RequestCancelled);
+      RequestCancelled,
+      ContentModified);
 
    type ResponseError is record
       code: ErrorCodes;
